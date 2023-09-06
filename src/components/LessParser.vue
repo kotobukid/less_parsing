61b65e202d759299d008de305abba143baeaf9a7
kotobukid <kotobuki@daqua.jp>
2023-09-06 19:41:34 +0900

    スタイルのインライン適用版と適用無し版でのレンダリングを試せるようにした ※build生成物は動作せず　postcssがnode向けであるため

--- src/components/LessParser.vue
+++ src/components/LessParser.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref} from "vue";
+import {type Ref, ref} from "vue";
 import less from "less";
 import * as postcss from 'postcss';
 
@@ -9,13 +9,16 @@ a {color: red;
 }
 .circle-a {fill: red; stroke: black; stroke-width: 2px;}
 .circle-b {fill: blue; stroke: white; stroke-width: 1px;}
+rect.back {fill: pink;}
 `);
 const parsed_text = ref("");
 
-const originalSVG = ref(null);
+const originalSVG: Ref<SVGSVGElement | null> = ref(null);
+const example1: Ref<HTMLImageElement | null> = ref(null);
+const example2: Ref<HTMLImageElement | null> = ref(null);
 
 const parse = () => {
-    less.render(source_text.value).then(({css, imports}: { css: string, imports: any[] }) => {
+    less.render(source_text.value).then(({css}: { css: string }) => {
         parsed_text.value = css;
 
         const my_style_element = document.getElementById('my-style-element');
@@ -28,40 +31,79 @@ const parse = () => {
             document.head.appendChild(mse);
         }
 
-        const cssRules: Record<string, Record<{ string: string }>> = {};
+        const cssRules: Record<string, Record<string, string>> = {};
         const root = postcss.parse(css);
         root.nodes.forEach(n => {
             const rule: Record<string, string> = {};
+            // @ts-ignore
             n.nodes.forEach(n2 => {
                 rule[n2.prop] = n2.value;
-                console.log(`${n2.prop}: ${n2.value};`)
+                // console.log(`${n2.prop}: ${n2.value};`)
             });
-            cssRules[n.selector] = rule;
+            // @ts-ignore
+            cssRules[<string>n.selector] = rule;
         });
 
-        const svg_source = originalSVG.value.outerHTML;
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(svg_source, 'image/svg+xml');
-        const clonedSvgElement = doc.documentElement;
+        if (originalSVG.value) {
+            const svg_source = originalSVG.value.outerHTML;
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(svg_source, 'image/svg+xml');
 
-        clonedSvgElement.querySelectorAll('*').forEach((element) => {
-            for (const selector in cssRules) {
-                if (element.matches(selector)) {
-                    const properties = cssRules[selector];
-                    for (const [property, value] of Object.entries(properties)) {
-                        // if (!element.style) {
-                        //     element.style = {};
-                        // }
-                        element.setAttribute(property, value);
+            // @ts-ignore
+            const cloned_svg_element: SVGSVGElement = <SVGSVGElement>doc.documentElement;
+            cloned_svg_element.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+            if (example1.value) {
+                render_as_bmp(example1, cloned_svg_element);
+            }
+
+            cloned_svg_element.querySelectorAll('*').forEach((element) => {
+                for (const selector in cssRules) {
+                    if (element.matches(selector)) {
+                        const properties = cssRules[selector];
+                        for (const [property, value] of Object.entries(properties)) {
+                            // if (!element.style) {
+                            //     element.style = {};
+                            // }
+                            element.setAttribute(property, <string>value);
+                        }
                     }
                 }
-            }
-        });
+            });
 
-        console.log(clonedSvgElement);
+            if (example2.value) {
+                render_as_bmp(example2, cloned_svg_element);
+            }
+
+        }
     }, (error: Error) => {
         console.error(error);
     });
+}
+
+const render_as_bmp = (elem_ref: Ref<HTMLImageElement | null>, svg_element: SVGSVGElement) => {
+    const svg_str = '<?xml version="1.0" encoding="utf-8"?>' + new XMLSerializer().serializeToString(svg_element);
+
+    const img = new Image();
+    const canvas = document.createElement('canvas');
+    const ctx: CanvasRenderingContext2D = canvas.getContext('2d')!;
+
+    img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0);
+        if (elem_ref.value) {
+
+            elem_ref.value.src = canvas.toDataURL('image/png');
+        }
+    };
+
+    img.onerror = (e) => {
+        console.error(e);
+    }
+
+    img.src = 'data:image/svg+xml;base64,' + btoa(svg_str);
+
 }
 
 </script>
@@ -70,13 +112,33 @@ const parse = () => {
 .less_parser
     .wrapper
         textarea.main_input(v-model="source_text")
-        a.button(href="#" @click.prevent="parse") parse
+        a.button.apply_less(href="#" @click.prevent="parse")
+            span Lessをパースし
+            br
+            span ページに適用
         textarea.main_output(v-model="parsed_text" readonly="readonly")
         br.clearfix
     br
-    svg.svg_sample(ref="originalSVG" width="400" height="200" viewBox="0 0 400 200")
-        circle.circle-a(r="40" cx="50" cy="50")
-        circle.circle-b(r="40" cx="250" cy="50")
+    table
+        tr
+            th
+                span オリジナルSVG要素
+                br
+                span (style要素使用)
+            td
+                svg.svg_sample(ref="originalSVG" width="300" height="120" viewBox="0 0 300 120")
+                    rect.back(x="0" y="0" width="300" height="120" fill="white")
+                    circle.circle-a(r="40" cx="50" cy="50")
+                    circle.circle-b(r="40" cx="250" cy="50")
+        tr
+            th
+                span Canvas経由でDataURL転写
+            td
+                img.render.example1(ref="example1")
+        tr
+            th span スクリプトでスタイルルールをインライン化
+            td
+                img.render.example2(ref="example2")
 
 </template>
 
@@ -92,9 +154,9 @@ const parse = () => {
         float: left;
     }
 
-    a {
+    a.apply_less {
         display: inline-block;
-        width: 60px;
+        width: 200px;
         text-align: center;
     }
 
@@ -102,7 +164,7 @@ const parse = () => {
 }
 
 .main_input, .main_output {
-    width: 400px;
+    width: 300px;
     height: 200px;
     border: 1px solid black;
     padding: 10px;
@@ -119,5 +181,24 @@ const parse = () => {
 
 .svg_sample {
     outline: 1px solid grey;
+}
+
+img.render {
+    outline: 1px solid grey;
+    width: 300px;
+    height: 120px;
+}
+
+rect.back {
+    fill: lightblue;
+}
+
+table {
+    table-layout: fixed;
+    border-collapse: collapse;
+}
+
+th, td {
+    border: 1px solid grey;
 }
 </style>
