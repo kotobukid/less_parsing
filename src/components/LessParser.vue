a550c8c7057b571ba5ea963b168b0a4b9ffb53b6
kotobukid <kotobuki@daqua.jp>
2023-09-06 18:16:41 +0900

    SVG要素の一つ一つにスタイルをインラインで適用する

--- src/components/LessParser.vue
+++ src/components/LessParser.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import {ref} from "vue";
 import less from "less";
+import * as postcss from 'postcss';
 
 const source_text = ref(`
 a {color: red;
@@ -11,10 +12,10 @@ a {color: red;
 `);
 const parsed_text = ref("");
 
+const originalSVG = ref(null);
+
 const parse = () => {
-    console.log(source_text.value);
-    less.render(source_text.value).then(({css, imports}: {css: string, imports: any[]}) => {
-        console.log(imports);
+    less.render(source_text.value).then(({css, imports}: { css: string, imports: any[] }) => {
         parsed_text.value = css;
 
         const my_style_element = document.getElementById('my-style-element');
@@ -27,6 +28,37 @@ const parse = () => {
             document.head.appendChild(mse);
         }
 
+        const cssRules: Record<string, Record<{ string: string }>> = {};
+        const root = postcss.parse(css);
+        root.nodes.forEach(n => {
+            const rule: Record<string, string> = {};
+            n.nodes.forEach(n2 => {
+                rule[n2.prop] = n2.value;
+                console.log(`${n2.prop}: ${n2.value};`)
+            });
+            cssRules[n.selector] = rule;
+        });
+
+        const svg_source = originalSVG.value.outerHTML;
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(svg_source, 'image/svg+xml');
+        const clonedSvgElement = doc.documentElement;
+
+        clonedSvgElement.querySelectorAll('*').forEach((element) => {
+            for (const selector in cssRules) {
+                if (element.matches(selector)) {
+                    const properties = cssRules[selector];
+                    for (const [property, value] of Object.entries(properties)) {
+                        // if (!element.style) {
+                        //     element.style = {};
+                        // }
+                        element.setAttribute(property, value);
+                    }
+                }
+            }
+        });
+
+        console.log(clonedSvgElement);
     }, (error: Error) => {
         console.error(error);
     });
@@ -42,9 +74,9 @@ const parse = () => {
         textarea.main_output(v-model="parsed_text" readonly="readonly")
         br.clearfix
     br
-    svg.svg_sample(width="400" height="200" viewBox="0 0 400 200")
-        circle.circle-a(r="40"  cx="50" cy="50")
-        circle.circle-b(r="40"  cx="250" cy="50")
+    svg.svg_sample(ref="originalSVG" width="400" height="200" viewBox="0 0 400 200")
+        circle.circle-a(r="40" cx="50" cy="50")
+        circle.circle-b(r="40" cx="250" cy="50")
 
 </template>
 
@@ -52,18 +84,23 @@ const parse = () => {
 .clearfix {
     clear: both;
 }
+
 .wrapper {
     width: 1000px;
-    &>* {
+
+    & > * {
         float: left;
     }
+
     a {
         display: inline-block;
         width: 60px;
         text-align: center;
     }
+
     margin-bottom: 10px;
 }
+
 .main_input, .main_output {
     width: 400px;
     height: 200px;
@@ -71,6 +108,7 @@ const parse = () => {
     padding: 10px;
     border-radius: 10px;
 }
+
 .main_input {
     margin-right: 10px;
 }
